871fa12bf5f1cf83891422fee15cd97f91b3cdeb
starudream <justwangsheng@gmail.com>
2021-12-20 16:42:44 +0800

    test: test

--- dt/slicex/slice_test.go
+++ dt/slicex/slice_test.go
@@ -5,21 +5,18 @@ import (
 )
 
 func TestToInterface(t *testing.T) {
-	var v interface{}
-	v = []string{"1", "2", "3"}
+	var v interface{} = []string{"1", "2", "3"}
 	t.Logf("%#v", ToInterface(v))
 }
 
 func TestToString(t *testing.T) {
-	var v interface{}
-	v = []int{1, 2, 3}
+	var v interface{} = []int{1, 2, 3}
 	t.Logf("%#v", ToInterface(v))
 	t.Logf("%#v", ToString(ToInterface(v)))
 }
 
 func TestToInt64(t *testing.T) {
-	var v interface{}
-	v = []int{1, 2, 3}
+	var v interface{} = []int{1, 2, 3}
 	t.Logf("%#v", ToInterface(v))
 	t.Logf("%#v", ToInt64(ToInterface(v)))
 }
