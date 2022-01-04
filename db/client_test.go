3e06b080b2621f44a2f0026254aeb52a03b8f542
starudream <justwangsheng@gmail.com>
2022-01-04 14:08:15 +0800

    test: test

--- db/client_test.go
+++ db/client_test.go
@@ -22,10 +22,25 @@ import (
 //       dsn: file:sqlite?mode=memory&cache=shared
 
 type User struct {
+	Meta
+	Name   string `gorm:"size:64"`
+	Email  string `gorm:"size:128"`
+	Mobile string `gorm:"size:20"`
+}
+
+func (User) TableName() string {
+	return "user1"
+}
+
+type UserD struct {
 	MetaD
 	Name   string `gorm:"size:64;uniqueIndex:uk_deleted_at"`
 	Email  string `gorm:"size:128"`
 	Mobile string `gorm:"size:20"`
+}
+
+func (UserD) TableName() string {
+	return "user2"
 }
 
 var ctx = token.New("*", "123456", 0).WithContext()
@@ -42,7 +57,19 @@ func user(names ...string) *User {
 	}
 }
 
-func TestDefault(t *testing.T) {
+func userD(names ...string) *UserD {
+	name := fmt.Sprintf("%s-%s-%d", "admin", crypto.RandString(6), time.Now().UnixNano())
+	if len(names) > 0 && names[0] != "" {
+		name = names[0]
+	}
+	return &UserD{
+		Name:   name,
+		Email:  "admin@example.com",
+		Mobile: "12312341234",
+	}
+}
+
+func TestDefaultWithMeta(t *testing.T) {
 	assert.NoError(t, Default().AutoMigrate(&User{}))
 
 	res := Default().Create(user())
@@ -50,7 +77,7 @@ func TestDefault(t *testing.T) {
 	assert.Equal(t, int64(1), res.RowsAffected)
 	assert.NoError(t, Default().WithContext(ctx).Create(user()).Error)
 	assert.NoError(t, Default().Clauses(OnConflict{DoNothing: true}).Create(user("1")).Error)
-	assert.NoError(t, Default().Clauses(OnConflict{Columns: []Column{{Name: "name"}, {Name: "deleted_at"}}, DoUpdates: Assignments(map[string]interface{}{"name": "user01"})}).Create(user("1")).Error)
+	assert.NoError(t, Default().Clauses(OnConflict{Columns: []Column{{Name: "id"}}, DoUpdates: Assignments(map[string]interface{}{"name": "user01"})}).Create(user("1")).Error)
 
 	res = Default().First(&User{})
 	assert.NoError(t, res.Error)
@@ -61,4 +88,25 @@ func TestDefault(t *testing.T) {
 
 	assert.NoError(t, Default().WithContext(ctx).Delete(&User{}, Eq{Column: "id", Value: "1"}).Error)
 	assert.NoError(t, Default().Unscoped().Find(&User{}).Error)
+}
+
+func TestDefaultWithMetaD(t *testing.T) {
+	assert.NoError(t, Default().AutoMigrate(&UserD{}))
+
+	res := Default().Create(userD())
+	assert.NoError(t, res.Error)
+	assert.Equal(t, int64(1), res.RowsAffected)
+	assert.NoError(t, Default().WithContext(ctx).Create(userD()).Error)
+	assert.NoError(t, Default().Clauses(OnConflict{DoNothing: true}).Create(userD("1")).Error)
+	assert.NoError(t, Default().Clauses(OnConflict{Columns: []Column{{Name: "name"}, {Name: "deleted_at"}}, DoUpdates: Assignments(map[string]interface{}{"name": "user01"})}).Create(userD("1")).Error)
+
+	res = Default().First(&UserD{})
+	assert.NoError(t, res.Error)
+	assert.Equal(t, int64(1), res.RowsAffected)
+	assert.Equal(t, gorm.ErrRecordNotFound, Default().Take(&UserD{}, Eq{Column: "id", Value: "2"}).Error)
+
+	assert.NoError(t, Default().WithContext(ctx).Model(&UserD{}).Where(Eq{Column: "id", Value: "1"}).Update("email", "user01@example.com").Error)
+
+	assert.NoError(t, Default().WithContext(ctx).Delete(&UserD{}, Eq{Column: "id", Value: "1"}).Error)
+	assert.NoError(t, Default().Unscoped().Find(&UserD{}).Error)
 }
--- go.mod
+++ go.mod
@@ -33,7 +33,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -42,13 +42,13 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.9.0 // indirect
-	github.com/jackc/pgx/v4 v4.14.0 // indirect
+	github.com/jackc/pgtype v1.9.1 // indirect
+	github.com/jackc/pgx/v4 v4.14.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
