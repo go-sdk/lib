7eb6c6ef567defafcf43ad5cec1657fa7b2a4ba6
starudream <justwangsheng@gmail.com>
2022-02-10 15:08:25 +0800

    test: context test

--- cx/context_test.go
+++ cx/context_test.go
@@ -5,19 +5,21 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
 )
 
 func TestContext(t *testing.T) {
-	ctx1 := FromContext()
-	ctx1.Set("a", "1")
-	ctx1.Append("a", "11")
+	ctx0 := metadata.NewIncomingContext(context.Background(), metadata.Pairs("a", "1", "a", "11"))
+
+	ctx1 := FromContext(ctx0)
+	ctx1.Append("a", "111")
 	ctx1.Set("b", "1")
-	assert.Equal(t, []string{"1", "11"}, ctx1.Get("a"))
+	assert.Equal(t, []string{"1", "11", "111"}, ctx1.Get("a"))
 	assert.Equal(t, []string{"1"}, ctx1.Get("b"))
 	assert.Equal(t, []string(nil), ctx1.Get("c"))
 
 	ctx2 := FromContext(ctx1)
-	assert.Equal(t, []string{"1", "11"}, ctx2.Get("a"))
+	assert.Equal(t, []string{"1", "11", "111"}, ctx2.Get("a"))
 	ctx2.Set("a", "2")
 	assert.Equal(t, []string{"2"}, ctx2.Get("a"))
 	assert.Equal(t, []string{"1"}, ctx2.Get("b"))
